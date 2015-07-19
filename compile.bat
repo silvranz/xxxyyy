78e58313494069425bddb55d32d5c264872d8c8d
silvranz <silvranz@gmail.com>
2015-07-19 19:23:56 +0700

    compiler batch

---
+++ compile.bat
@@ -0,0 +1,14 @@
+set Tomcatpath="D:\Programming\xampp\tomcat\webapps"
+cd WEB-INF/classes/com/fresearch/oversign
+javac -classpath ../../../../lib/* controller/*.java
+TIMEOUT /T 10
+javac -classpath ../../../../lib/* data/*.java
+TIMEOUT /T 10
+javac -classpath ../../../../lib/* model/*.java
+TIMEOUT /T 10
+javac -classpath ../../../../lib/* parameter/*.java
+TIMEOUT /T 10
+javac -classpath ../../../../lib/* utility/*.java
+TIMEOUT /T 10
+cd /d %Tomcatpath%
+jar -cvf oversign.war *
