db3f7ab1ffb5070640a8301f3dc708fd865c3825
Angela Muliawan <amuliawan93@gmail.com>
2015-07-22 21:57:43 +0700

    PopularHashtagObj

---
+++ WEB-INF/classes/com/fresearch/oversign/data/PopularHashtagObj.java
@@ -0,0 +1,26 @@
+package com.fresearch.oversign;
+
+public class PopularHashtagObj {
+	private String userid;
+	private String useremail;
+	private String userfullname;
+	
+	public String getUserID() {
+		return userid;
+	}
+	public void setUserID(String userid) {
+		this.userid = userid;
+	}
+	public String getUserEmail() {
+		return useremail;
+	}
+	public void setUserEmail(String useremail) {
+		this.useremail = useremail;
+	}
+	public String getUserFullName() {
+		return userfullname;
+	}
+	public void setUserFullName(String userfullname) {
+		this.userfullname = userfullname;
+	}
+}
